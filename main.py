83e2584776f809a7d9a334c564821b7e08a5224e
Ahmad Alismail <46696930+afi1289@users.noreply.github.com>
2023-06-29 17:14:34 +0200

    add streamlit app

--- main.py
+++ main.py
@@ -1,13 +1,21 @@
-from fastapi import FastAPI
+import streamlit as st
+import numpy as np
+import pandas as pd
 
-app = FastAPI()
+st.set_page_config(page_title="Podcast Analyzer", page_icon=":robot:")
 
+st.image(image='imgs\podcast_analyzer.png')
 
-@app.get("/users/me")
-async def read_user_me():
-    return {"user_id": "the current user"}
+def get_input_type():
+    input_type = st.radio("Set your input type", label_visibility='collapsed', key="Podcast URL", 
+                          options=["Podcast Video", "Transcript File"], horizontal = True)
+    return input_type
 
+input_type = get_input_type()
 
-@app.get("/users/{user_id}")
-async def read_user(user_id: str):
-    return {"user_id": user_id}
+def get_text():
+    input_text = st.text_input(label="Podcast label", label_visibility='collapsed', placeholder=f"Your {input_type} URL...", key="podcast_input")
+    return input_text
+
+podcast_input = get_text()
+
