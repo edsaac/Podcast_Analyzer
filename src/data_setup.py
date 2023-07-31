91cb00e18644770c213443a657998a11ea7ff1f1
Edwin <34405119+edsaac@users.noreply.github.com>
2023-07-31 07:51:17 -0500

    Update data_setup.py

--- src/data_setup.py
+++ src/data_setup.py
@@ -41,7 +41,7 @@ def download_audio(link, quality='64'):
             'preferredcodec': 'mp3',
             'preferredquality': quality,
         }],
-        'ffmpeg_location': '.venv/ffmpeg-master-latest-win64-gpl/bin' 
+        'ffmpeg_location': "" 
     }) as video:
         info_dict = video.extract_info(link, download=True)
         video_id = info_dict['id']
